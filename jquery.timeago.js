afba14838ccaedeb1e3f9343a774c0e602b3863d
Annabel Church <annabel.church@guardian.co.uk>
2012-03-13 16:40:27 +0000

    Providing more strict wording for hours (1 hour and 3 minutes), and adapt threasholds to suit.

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -29,6 +29,7 @@
     settings: {
       refreshMillis: 60000,
       allowFuture: false,
+      strict: false, // more strict guardian wording for fuzzy times
       strings: {
         prefixAgo: null,
         prefixFromNow: null,
@@ -47,10 +48,30 @@
         years: "%d years",
         wordSeparator: " ",
         numbers: []
+      },
+      stringsStrict: {
+        prefixAgo: null,
+        prefixFromNow: null,
+        suffixAgo: "ago",
+        suffixFromNow: "from now",
+        seconds: "less than a minute",
+        minute: "a minute",
+        minutes: "%d minutes", 
+        hour: "1 hour",
+        hours: "%d hours",
+        day: "a day",
+        days: "%d days",
+        month: "a month",
+        months: "%d months",
+        year: "a year",
+        years: "%d years",
+        timeSeparator: "and",
+        wordSeparator: " ",
+        numbers: []
       }
     },
     inWords: function(distanceMillis) {
-      var $l = this.settings.strings;
+      var $l = this.settings.strict ? this.settings.stringsStrict : this.settings.strings;
       var prefix = $l.prefixAgo;
       var suffix = $l.suffixAgo;
       if (this.settings.allowFuture) {
@@ -63,6 +84,7 @@
       var seconds = Math.abs(distanceMillis) / 1000;
       var minutes = seconds / 60;
       var hours = minutes / 60;
+      var remainingMinutes = minutes % 60;
       var days = hours / 24;
       var years = days / 365;
 
@@ -72,11 +94,19 @@
         return string.replace(/%d/i, value);
       }
 
+      function remainderInWords(singular, plural, remainder){
+        if(remainder == 0){
+          return ''; // early out
+        }
+        var words = remainder == 1 ? substitute(singular, remainder) : substitute(plural, remainder);
+        return $l.wordSeparator + $l.timeSeparator + $l.wordSeparator + words;
+      }
+
       var words = seconds < 45 && substitute($l.seconds, Math.round(seconds)) ||
-        seconds < 90 && substitute($l.minute, 1) ||
-        minutes < 45 && substitute($l.minutes, Math.round(minutes)) ||
-        minutes < 90 && substitute($l.hour, 1) ||
-        hours < 24 && substitute($l.hours, Math.round(hours)) ||
+        seconds < 45 && substitute($l.minute, 1) ||
+        minutes < 60 && substitute($l.minutes, Math.round(minutes)) ||
+        minutes < 120 && substitute($l.hour, 1) + remainderInWords($l.minute , $l.minutes, Math.round(remainingMinutes))  ||       
+        hours < 24 && substitute($l.hours, Math.round(hours)) + remainderInWords($l.minute , $l.minutes, Math.round(remainingMinutes)) ||
         hours < 42 && substitute($l.day, 1) ||
         days < 30 && substitute($l.days, Math.round(days)) ||
         days < 45 && substitute($l.month, 1) ||
